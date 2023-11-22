01bc8c3cf83e7bfe563dc2ed78311efb0e9b4646
sthandiwe3561 <sthandiwemsweli35@gmail.com>
2023-11-22 14:28:05 +0200

    updated app.css and index.js

--- my-reacts-app/src/index.js
+++ my-reacts-app/src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="paragraph">
+      This project was coded by, <a href="/">Sthandiwe Msweli</a> and is{" "}
+      <a href="https://github.com/sthandiwe3561/weather-app">
+        Open-source code
+      </a>{" "}
+      and{" "}
+      <a href="https://gentle-medovik-598532.netlify.app/">hosted on Netlify</a>
+    </div>
   </React.StrictMode>
 );
 
